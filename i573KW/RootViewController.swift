ae3ede5540168b71e83d7027e293ff3ed3606f1e
Weiyi Kong <kongwy@users.noreply.github.com>
2021-05-29 15:46:01 +0800

    chore: init project

---
+++ i573KW/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  i573KW
+//
+//  Created by Weiyi Kong on 29/5/21.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
